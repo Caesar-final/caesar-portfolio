2111ed4fb648ed58c91e1de52d36826c846898da
cesa <johnvictorcandrade@gmail.com>
2025-11-27 22:00:28 -0300

    Updates

--- _next/static/p77M0VV6NUAZGWtnIHHKE/_buildManifest.js
+++ _next/static/p77M0VV6NUAZGWtnIHHKE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/caesar-portfolio//_next/:path+",
+        "destination": "/caesar-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
